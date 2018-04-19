1c268cd132ba3a03c0150194b511c4b6a0e61ceb
wangjing <wangjing>
2018-04-19 14:03:17 +0800

    first commit

--- app/src/main/java/com/wangjing/retrofitutils/RetrofitBuilder.java
+++ app/src/main/java/com/wangjing/retrofitutils/RetrofitBuilder.java
@@ -7,10 +7,14 @@ package com.wangjing.retrofitutils;
  * 最近修改：2018/4/17 15:23 by WangJing
  */
 public class RetrofitBuilder {
+
     /**
      * 请求的baseUrl
      */
     private String baseUrl;
+    private int connectTimeout;
+    private int readTimeout;
+    private int writeTimeout;
 
 
     /**
@@ -21,6 +25,36 @@ public class RetrofitBuilder {
      */
     public RetrofitBuilder baseUrl(String baseUrl) {
         this.baseUrl = baseUrl;
+        return this;
+    }
+
+    /**
+     * 设置网络请求的连接超时时间
+     * @param connectTimeout
+     * @return
+     */
+    public RetrofitBuilder connectTimeout(int connectTimeout) {
+        this.connectTimeout = connectTimeout;
+        return this;
+    }
+
+    /**
+     * 设置网络请求的读取超时时间
+     * @param readTimeout
+     * @return
+     */
+    public RetrofitBuilder readTimeout(int readTimeout) {
+        this.readTimeout = readTimeout;
+        return this;
+    }
+
+    /**
+     * 设置网络请求的写入超时时间
+     * @param writeTimeout
+     * @return
+     */
+    public RetrofitBuilder writeTimeout(int writeTimeout) {
+        this.writeTimeout = writeTimeout;
         return this;
     }
 
