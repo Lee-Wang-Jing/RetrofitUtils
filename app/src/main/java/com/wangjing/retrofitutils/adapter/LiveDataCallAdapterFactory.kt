89658a24017c80dbaaa2c2a5e7525d7fcc033731
wangjing <919335417@qq.com>
2019-10-21 11:40:42 +0800

    支持LiveData和Retrofit结合

--- app/src/main/java/com/wangjing/retrofitutils/adapter/LiveDataCallAdapterFactory.kt
+++ app/src/main/java/com/wangjing/retrofitutils/adapter/LiveDataCallAdapterFactory.kt
@@ -15,16 +15,24 @@ class LiveDataCallAdapterFactory : Factory() {
         annotations: Array<Annotation>,
         retrofit: Retrofit
     ): CallAdapter<*, *>? {
+        //获取returnType的class类型
         if (Factory.getRawType(returnType) != LiveData::class.java) {
+            //throw IllegalArgumentException("返回值不是LiveData类型")
             return null
         }
+        //先解释一下getParameterUpperBound
+        //官方例子
+        //For example, index 1 of {@code Map<String, ? extends Runnable>} returns {@code Runnable}.
+        //获取的是Map<String,? extends Runnable>参数列表中index序列号的参数类型,即0为String,1为Runnable
+        //这里的0就是LiveData<?>中?的序列号,因为只有一个参数
+        //其实这个就是我们请求返回的实体
         val observableType = Factory.getParameterUpperBound(0, returnType as ParameterizedType)
         val rawObservableType = Factory.getRawType(observableType)
         if (rawObservableType != ApiResponse::class.java) {
             throw IllegalArgumentException("type must be a resource")
         }
         if (observableType !is ParameterizedType) {
-            throw IllegalArgumentException("resource must be parameterized")
+            throw IllegalArgumentException("返回值需为参数化类型")
         }
         val bodyType = Factory.getParameterUpperBound(0, observableType)
         return LiveDataCallAdapter<Any>(bodyType)
